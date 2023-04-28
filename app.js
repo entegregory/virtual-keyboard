f480d0f4c589a4fff648d23c92e7496820844711
entegregory <entegregory@gmail.com>
2023-04-29 00:54:44 +0600

    feat: сreate keyboard container, add the stylesheet link

---
+++ app.js
@@ -0,0 +1,8 @@
+const link = document.createElement('link');
+link.rel = 'stylesheet';
+link.href = 'style.css';
+document.head.appendChild(link);
+
+const keyboardContainer = document.createElement('div');
+keyboardContainer.id = 'keyboard-container';
+document.body.appendChild(keyboardContainer);
