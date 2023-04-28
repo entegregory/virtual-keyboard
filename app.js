0ec784877d34fdd612f7cbb07a83fcf288f8718d
entegregory <entegregory@gmail.com>
2023-04-29 01:25:57 +0600

    feat: create VirtualKeyboard class and initialize it with keyboard container

--- app.js
+++ app.js
@@ -6,3 +6,23 @@ document.head.appendChild(link);
 const keyboardContainer = document.createElement('div');
 keyboardContainer.id = 'keyboard-container';
 document.body.appendChild(keyboardContainer);
+
+class VirtualKeyboard {
+  constructor(container) {
+    this.keyboardContainer = container;
+    this.supportedLayouts = ['en', 'ru'];
+    this.layout = {
+      en: [],
+      ru: [],
+    };
+    this.currentLayout = localStorage.getItem('layout') || 'en';
+    this.init();
+  }
+
+  init() {
+    this.generateKeys();
+  }
+
+  generateKeys() {
+  }
+}
